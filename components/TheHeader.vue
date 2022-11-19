6d0c45c4c577c9a5b1a53385758fc864228c70a0
Vadim Ekimov <ekimov_vd@mail.ru>
2022-11-19 19:43:21 +0700

    Добавил компонент хедера

---
+++ components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="the-header">
+        <div class="the-header__container">
+            <img
+                class="the-header__logo"
+                src="@/assets/images/header/logo.svg"
+                alt="doclub"
+            />
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.the-header {
+    background: #2c307e;
+    padding: 32px 0 142px 0;
+
+    &__container {
+        @include container-full;
+        display: flex;
+        justify-content: center;
+    }
+}
+
+@media screen and (max-width: 768px) {
+    .the-header {
+        padding: 11px 0 94px 0;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+    <TheHeader />
     <TheRegistrationBanner />
     <TheFooter />
 </template>
